ae990de2a4c1d9614d9032c72620aef0d3a12b89
Noah <noahwillen@gmail.com>
2024-02-09 18:47:08 -0500

    Initial commit

---
+++ src/main/java/net/noah/nolavapockets/NoLavaPockets.java
@@ -0,0 +1,11 @@
+package net.noah.nolavapockets;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(NoLavaPockets.MOD_ID)
+public class NoLavaPockets {
+    public static final String MOD_ID = "nolavapockets";
+    public NoLavaPockets () {
+
+    }
+}
